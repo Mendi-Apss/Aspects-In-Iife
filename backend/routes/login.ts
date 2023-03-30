b1c829a1881f0d24450de15099eb911fa3490678
mendi <mmfriedman16@gmail.com>
2023-03-30 15:51:59 +0300

    added login router

---
+++ backend/routes/login.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import StatusCodes from 'http-status-codes';
+
+const router = express.Router();
+
+router.get('/', (request, response) => {
+    response
+        .status(StatusCodes.OK)
+        .send('get request succeeded')
+
+});
+
+router.post('/', (request, response) => {
+
+});
+
+router.put('/', (request, response) => {
+
+});
+
+router.delete('/', (request, response) => {
+
+});
+
+module.exports = router;
