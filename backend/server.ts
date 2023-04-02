fee435f996d7c06bbf7bf7f19fcd5094944eaa54
mendi <mmfriedman16@gmail.com>
2023-04-02 20:38:31 +0300

    added sign in route

--- backend/server.ts
+++ backend/server.ts
@@ -2,9 +2,11 @@ import express, { Express } from 'express';
 const app: Express = express();
 const PORT = 2121;
 
-const loginRoute = require('./routes/login');
+const LOGIN_ROUTE = require('./routes/login');
+const SIGN_IN_ROUTE = require('./routes/sign-in');
 
-app.use('/login', loginRoute);
+app.use('/login', LOGIN_ROUTE);
+app.use('/sign-in',SIGN_IN_ROUTE);
 
 
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
